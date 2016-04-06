f37a6af2f05e7cbc13695a1f27a82dad7b7d96a6
SmthDiff Designs <designs@smthdiff.com>
2016-04-06 22:37:44 +0200

    [TASK] basic package setup

---
+++ src/Provider/AzuriteServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SmthDiff\Azurite\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AzuriteServiceProvider extends ServiceProvider
+{
+    public function boot() {
+        // TODO: Implement boot() method.
+    }
+
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
